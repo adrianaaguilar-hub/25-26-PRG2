bbfe0619aac387021d0b09c34027c1adf4a0dc1e
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-02-17 13:40:47 +0100

    feat: implementar formato de tablero con encabezados y símbolos
    - Agregar separadores con signos de igual arriba y abajo del tablero
    - Agregar encabezado de columnas (1-7) y números de fila (1-5)
    - Cambiar símbolos: - para sin revelar, .  para libre, *  para mina
    - Agregar espacios entre casillas para mejor legibilidad

--- entregas/aguilarAdriana/src/reto003/BuscaMinas.java
+++ entregas/aguilarAdriana/src/reto003/BuscaMinas.java
@@ -62,12 +62,16 @@ public class BuscaMinas {
     
  
     static void imprimir ( int[][] mapa) {
+        System.out.println("================");
+        System.out.println("  1 2 3 4 5 6 7");
         for (int i = 0; i < mapa.length; i++) {
+            System.out.print((i + 1) + " ");
             for (int j = 0; j < mapa[i].length; j++){
-                System.out.print(mapear(mapa[i][j]));
+                System.out.print(mapear(mapa[i][j]) + " ");
             }
             System.out.println();
         }
+        System.out.println("================");
     }
     
     
@@ -75,9 +79,9 @@ public class BuscaMinas {
     static String mapear( int casilla) {
 
         String[] simbolos = {
-            "?",
-            "~",
-            "|o/"
+            "-",
+            ".",
+            "*"
         };
         
         return casilla < 0 ? simbolos[0] : simbolos[casilla];
