ecb2cdaaed0c33a3d258c2ff0818e8ca80c6a937
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-03-17 13:51:20 +0100

    feat: creación de clases Cliente y Fraccion con implementación de métodos en clase Fraccion

---
+++ evaluaciones/retos/reto004/Fraccion.java
@@ -0,0 +1,48 @@
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public Fraccion(int entero) {
+        this.numerador = entero;
+        this.denominador = 1;
+    }
+
+
+    public void sumar(Fraccion fraccion) {
+        
+    }
+    public void restar(Fraccion fraccion){
+        
+    }
+    public void multiplicar(Fraccion fraccion){
+        
+    }
+    public void dividir(Fraccion fraccion){
+        
+    }
+
+
+    public boolean menorQue(Fraccion fraccion){
+
+    }
+    public boolean mayorQue(Fraccion fraccion){
+
+    }
+
+
+    public boolean equals(Fraccion fraccion){
+
+    }
+
+
+
+    simplificar(){
+
+    }
+}
