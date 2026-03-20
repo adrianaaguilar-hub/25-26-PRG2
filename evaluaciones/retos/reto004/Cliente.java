fae9d7fbed423bfc7084403607bbb58f730212ee
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-03-20 09:20:10 +0100

    feat: agregar prueba de equals en Cliente

--- evaluaciones/retos/reto004/Cliente.java
+++ evaluaciones/retos/reto004/Cliente.java
@@ -9,5 +9,10 @@ public class Cliente {
 
     System.out.println("menorQue (1/2 < 3/4): " + a.menorQue(b));
     System.out.println("mayorQue (1/2 > 3/4): " + a.mayorQue(b));
+
+    Fraccion c = new Fraccion(2, 4);
+    Fraccion d = new Fraccion(1, 2);
+
+    System.out.println("equals (2/4 == 1/2): " + c.equals(d));
 	}
 }
