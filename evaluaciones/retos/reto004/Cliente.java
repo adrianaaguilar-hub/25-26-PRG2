1128dee4fbbaf2a663f56b3d9fc291e972b5dbe3
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-03-20 09:19:30 +0100

    feat: agregar pruebas de menorQue y mayorQue en Cliente

--- evaluaciones/retos/reto004/Cliente.java
+++ evaluaciones/retos/reto004/Cliente.java
@@ -3,5 +3,11 @@ public class Cliente {
 	
 	public static void main(String[] args) {
     System.out.println("=== Pruebas clase Fraccion ===");
+
+    Fraccion a = new Fraccion(1, 2);
+    Fraccion b = new Fraccion(3, 4);
+
+    System.out.println("menorQue (1/2 < 3/4): " + a.menorQue(b));
+    System.out.println("mayorQue (1/2 > 3/4): " + a.mayorQue(b));
 	}
 }
