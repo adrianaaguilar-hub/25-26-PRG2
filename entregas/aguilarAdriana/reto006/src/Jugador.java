35fa92d9154a67163af858d53a23d95a886a2970
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-05-08 11:28:07 +0200

    feat: vista publica en clase Jugador

--- entregas/aguilarAdriana/reto006/src/Jugador.java
+++ entregas/aguilarAdriana/reto006/src/Jugador.java
@@ -1,3 +1,11 @@
 public class Jugador {
-    
+    private Mano mano; 
+
+    public Jugador () {}
+
+    public int consultarPuntaje () {}
+
+    public void recibirCartas () {}
+
+    public boolean seHaPasado () {}
 }
