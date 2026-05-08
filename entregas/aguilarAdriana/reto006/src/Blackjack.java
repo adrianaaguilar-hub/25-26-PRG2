875ae3ab99b6c174f333682f75876e64be74c975
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-05-08 11:47:15 +0200

    feat: vista publica en clase Blackjack

--- entregas/aguilarAdriana/reto006/src/Blackjack.java
+++ entregas/aguilarAdriana/reto006/src/Blackjack.java
@@ -1,3 +1,9 @@
 public class Blackjack {
-    
+    private Baraja baraja;
+    private Jugador jugador;
+    private Console console;
+
+    public Blackjack () {}
+
+    public void jugar () {}
 }
