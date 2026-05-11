706fcf6fd75dbeb3402c75c48af684be63d9dd78
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-05-11 18:32:10 +0200

    feat: clase mano: Método	Qué hace añadir(Carta)	Agrega una carta al final del array (incrementa contador) sumaTotal()	Calcula los puntos totales (esto es complejo, ver abajo) tiene21()	Devuelve true si la suma es exactamente 21 mostrar()	Imprime todas las cartas + la suma total

--- entregas/aguilarAdriana/reto006/src/Mano.java
+++ entregas/aguilarAdriana/reto006/src/Mano.java
@@ -1,24 +1,32 @@
 public class Mano {
-    private Carta [] cartas;
+    private Carta[] cartas;
     private int contador;
+    private Console console;
 
-    public Mano () {
-
+    public Mano() {
+        this.cartas = new Carta[21];  
+        this.contador = 0;
+        this.console = new Console();
     }
 
-    public void añadir (Carta carta) {}
+    public void añadir(Carta carta) {
+        if (this.contador < this.cartas.length) {
+            this.cartas[this.contador] = carta;
+            this.contador++;
+        }
+    }
 
-    public int sumaTotal () {
+    public int sumaTotal() {
         int suma = 0;
         int ases = 0;
 
-       
         for (int i = 0; i < contador; i++) {
             suma += cartas[i].valor21();
             if (cartas[i].esAs()) {
                 ases++;
             }
         }
+
 
         while (ases > 0 && suma + 10 <= 21) {
             suma += 10;
@@ -28,9 +36,20 @@ public class Mano {
         return suma;
     }
 
-    public boolean tiene21 () {}
+    public boolean tiene21() {
+        return this.sumaTotal() == 21;
+    }
 
-    public boolean esMenora21 () {}
+    public boolean esMenora21() {
+        return this.sumaTotal() < 21;
+    }
 
-    public void mostrar () {}
+    public void mostrar() {
+        this.console.write("Mano: ");
+        for (int i = 0; i < this.contador; i++) {
+            this.cartas[i].mostrar();
+            this.console.write(" ");
+        }
+        this.console.writeln("| Suma: " + this.sumaTotal());
+    }
 }
